6cb37a831be187181e211da65931046df0594216
Michel Meyer <meyer_michel@outlook.com>
2022-10-14 13:43:54 +0000

    setup database

--- backend/example/setup.py
+++ backend/example/setup.py
@@ -1,40 +1,83 @@
 from immudb import ImmudbClient
 from uuid import uuid4
 
-URL = "localhost:3322"  # ImmuDB running on your machine
-LOGIN = "immudb"        # Default username
-PASSWORD = "immudb"     # Default password
-DB = b"defaultdb"       # Default database name (must be in bytes)
+URL = "database:3322"  # ImmuDB running on your machine
+LOGIN = "immudb"  # Default username
+PASSWORD = "immudb"  # Default password
+DB = b"defaultdb"  # Default database name (must be in bytes)
+
 
 def main():
     client = ImmudbClient(URL)
-    client.login(LOGIN, PASSWORD, database = DB)
+    client.login(LOGIN, PASSWORD, database=DB)
 
-    client.sqlExec("""
-        CREATE TABLE IF NOT EXISTS systemToTools(
-    system_id INT,
-    tool_id INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (system_id, tool_id)
-);""")
-        
-    client.sqlExec("""
-        CREATE TABLE IF NOT EXISTS entity(
+    client.sqlExec(
+        """ 
+        CREATE TABLE IF NOT EXISTS systemtool ( 
+        id INTEGER AUTO_INCREMENT,
+        system_id VARCHAR[64], 
+        tool_id VARCHAR[64],
+        created_at TIMESTAMP,
+        PRIMARY KEY (id)
+        );"""
+    )
+
+    client.sqlExec(
+        """
+        CREATE TABLE IF NOT EXISTS entity (
     id INTEGER AUTO_INCREMENT,
+    uuid VARCHAR[64],
     name VARCHAR,
     version VARCHAR,
     purpose VARCHAR,
-    changed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    changed_at TIMESTAMP,
     is_system BOOLEAN,
     PRIMARY KEY id
-);""")
+);"""
+    )
 
-    client.sqlExec("""
-        CREATE TABLE IF NOT EXISTS change(
-    id INTEGER AUTO_INCREMENT,
+    client.sqlExec(
+        """
+        CREATE TABLE IF NOT EXISTS change (
+            id INTEGER AUTO_INCREMENT,
     change VARCHAR,
-    entity_id INT,
-);""")
+    entity_id VARCHAR[64],
+    PRIMARY KEY id
+);"""
+    )
+
+    uuid_gcc = str(uuid4())
+    uuid_clang = str(uuid4())
+    uuid_system1 = str(uuid4())
+
+    resp = client.sqlExec(
+        f"""
+        BEGIN TRANSACTION;
+
+        INSERT INTO entity (name, version, uuid, purpose, changed_at, is_system) 
+            VALUES ('GCC', '1.0.0', '{uuid_gcc}','Compiler for C/C++', NOW(), FALSE);
+        INSERT INTO entity (name, version, uuid, purpose, changed_at, is_system)
+           VALUES ('CLANG', '10.0.0','{uuid_clang}', 'Compiler for C/C++', NOW(), FALSE);
+       INSERT INTO entity (name, version, uuid, purpose, changed_at, is_system)
+           VALUES ('SYSTEM1', '1.0.0', '{uuid_system1}', 'Toolchain for embedded devices', NOW(), TRUE);
+       INSERT INTO systemtool (system_id, tool_id, created_at)
+           VALUES ('{uuid_system1}', '{uuid_clang}', NOW());
+       INSERT INTO systemtool (system_id, tool_id, created_at)
+           VALUES ('{uuid_system1}', '{uuid_gcc}', NOW());
+        
+        COMMIT;
+    """
+    )
+
 
 if __name__ == "__main__":
     main()
+
+#
+#
+#
+#
+#
+#
+#
+#
