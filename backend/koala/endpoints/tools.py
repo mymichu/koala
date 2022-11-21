0b0d35dfb90cb453a8acc5793d54f9282ae0075e
Michel Meyer <michel.meyer@ypsomed.com>
2022-11-21 07:10:10 +0000

    fix python checks

--- backend/koala/endpoints/tools.py
+++ backend/koala/endpoints/tools.py
@@ -1,4 +1,5 @@
 import json
+from typing import Dict
 
 from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends
@@ -11,16 +12,16 @@ router = APIRouter()
 
 @router.get("/tools/", tags=["tools"])
 @inject
-async def read_all_tools(tool_api: ToolApi = Depends(Provide[ContainerApi.api_tool_factory])):
+async def read_all_tools(tool_api: ToolApi = Depends(Provide[ContainerApi.api_tool_factory])) -> str:
     tools = tool_api.get_all_tools()
     return json.dumps(tools)
 
 
 @router.get("/users/me", tags=["users"])
-async def read_user_me():
+async def read_user_me() -> Dict[str, str]:
     return {"username": "fakecurrentuser"}
 
 
 @router.get("/users/{username}", tags=["users"])
-async def read_user(username: str):
+async def read_user(username: str) -> Dict[str, str]:
     return {"username": username}
